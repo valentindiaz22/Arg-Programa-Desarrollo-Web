e7a071901c7bc97f91ca60e182f72d8aaf28e0e6
Valentin Diaz <114015317+valentindiaz22@users.noreply.github.com>
2023-02-24 17:46:25 -0300

    TP1

---
+++ Clase1 ARG Programa.js
@@ -0,0 +1,20 @@
+var auto = "Corolla";
+console.log(auto);
+console.log(typeof(auto));
+
+var auto = 308
+console.log(auto);
+console.log(typeof(auto));
+
+var auto = "C4"
+console.log(auto);
+console.log(typeof(auto));
+
+var nombre = "Valentin";
+var apellido = "Diaz";
+console.log(nombre, apellido);
+
+var fruta = "Naranja";
+var tipoDeLaVariableFruta = typeof(fruta);
+console.log(tipoDeLaVariableFruta);
+
